fa88ff8eb119d82f9411cbdd207ef342e612f63a
Negar <negarsourati@gmail.com>
2022-06-06 16:30:08 +0430

    cmake

---
+++ CMakeFiles/CHESS.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/oem/Downloads/CHESS/src/Bishop.cpp" "/home/oem/Downloads/CHESS/CMakeFiles/CHESS.dir/src/Bishop.cpp.o"
+  "/home/oem/Downloads/CHESS/src/King.cpp" "/home/oem/Downloads/CHESS/CMakeFiles/CHESS.dir/src/King.cpp.o"
+  "/home/oem/Downloads/CHESS/src/Knight.cpp" "/home/oem/Downloads/CHESS/CMakeFiles/CHESS.dir/src/Knight.cpp.o"
+  "/home/oem/Downloads/CHESS/src/Pawn.cpp" "/home/oem/Downloads/CHESS/CMakeFiles/CHESS.dir/src/Pawn.cpp.o"
+  "/home/oem/Downloads/CHESS/src/Queen.cpp" "/home/oem/Downloads/CHESS/CMakeFiles/CHESS.dir/src/Queen.cpp.o"
+  "/home/oem/Downloads/CHESS/src/Rook.cpp" "/home/oem/Downloads/CHESS/CMakeFiles/CHESS.dir/src/Rook.cpp.o"
+  "/home/oem/Downloads/CHESS/src/board.cpp" "/home/oem/Downloads/CHESS/CMakeFiles/CHESS.dir/src/board.cpp.o"
+  "/home/oem/Downloads/CHESS/src/main.cpp" "/home/oem/Downloads/CHESS/CMakeFiles/CHESS.dir/src/main.cpp.o"
+  "/home/oem/Downloads/CHESS/src/pieces.cpp" "/home/oem/Downloads/CHESS/CMakeFiles/CHESS.dir/src/pieces.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/CHESS.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CHESS"
+  "CHESS.pdb"
+  "CMakeFiles/CHESS.dir/src/Bishop.cpp.o"
+  "CMakeFiles/CHESS.dir/src/King.cpp.o"
+  "CMakeFiles/CHESS.dir/src/Knight.cpp.o"
+  "CMakeFiles/CHESS.dir/src/Pawn.cpp.o"
+  "CMakeFiles/CHESS.dir/src/Queen.cpp.o"
+  "CMakeFiles/CHESS.dir/src/Rook.cpp.o"
+  "CMakeFiles/CHESS.dir/src/board.cpp.o"
+  "CMakeFiles/CHESS.dir/src/main.cpp.o"
+  "CMakeFiles/CHESS.dir/src/pieces.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CHESS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
